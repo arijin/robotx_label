61a3edf29702d76e32db4bbde445cbf5fdf132d1
arijin <arijin@163.com>
2022-01-26 21:35:18 +0800

    modify the needed raw information style to make the tool can generated timestamp-aligned label.

--- label_gazebo/scripts/label.py
+++ label_gazebo/scripts/label.py
@@ -33,7 +33,7 @@ split_dir = os.path.join(root_dir, split)
 
 
 class raw_object(object):
-    def __init__(self, mystate):
+    def __init__(self):
         self.type = None
         self.tx = None
         self.ty = None
@@ -47,13 +47,10 @@ class raw_object(object):
 
         self.tyaw = None
 
-        self.mx = mystate.modelstate.pose.position.x
-        self.my = mystate.modelstate.pose.position.y
-        self.mz = mystate.modelstate.pose.position.z
+        self.tlinearx = None
+        self.tangularz = None
 
-        self.mroll = self.get_state_rpy(mystate)[0]
-        self.mpitch = self.get_state_rpy(mystate)[1]
-        self.myaw = self.get_state_rpy(mystate)[2]
+        self.timestamp = None
 
     def read_target_from_state(self, type, objectstate):
         self.type = type
@@ -61,9 +58,16 @@ class raw_object(object):
         self.ty = objectstate.modelstate.pose.position.y
         self.tz = objectstate.modelstate.pose.position.z
 
-        self.tyaw = self.get_state_rpy(objectstate)[2]
+        qx = objectstate.modelstate.pose.orientation.x
+        qy = objectstate.modelstate.pose.orientation.y
+        qz = objectstate.modelstate.pose.orientation.z
+        qw = objectstate.modelstate.pose.orientation.w
+        self.tyaw = gp.quart_to_rpy(qx, qy, qz, qw)[2]
 
-        self.tdis = gp.euclid_distance(self.mx, self.my, self.tx, self.ty)
+        self.tlinearx = objectstate.modelstate.twist.linear.x
+        self.tangularz = objectstate.modelstate.twist.angular.z
+
+        self.timestamp = objectstate.header.stamp
 
     def read_target_from_param(self, type, tx, ty, global_yaw):
         self.type = type
@@ -71,20 +75,14 @@ class raw_object(object):
         self.ty = ty
         self.tz = 0
 
-        # unfinished.
         if type == "cone_buoy" or type == "sphere_buoy":
             self.tyaw = 0
         else:
             self.tyaw = global_yaw
-        self.tdis = gp.euclid_distance(self.mx, self.my, self.tx, self.ty)
 
-    def get_state_rpy(self, state):
-        qx = state.modelstate.pose.orientation.x
-        qy = state.modelstate.pose.orientation.y
-        qz = state.modelstate.pose.orientation.z
-        qw = state.modelstate.pose.orientation.w
-        rpy = gp.quart_to_rpy(qx, qy, qz, qw)
-        return rpy
+        self.tlinearx = 0
+        self.tangularz = 0
+        self.timestamp = 0
 
     def read_basicinfos(self, type, subtype):
         ship_size = rospy.get_param(f"{type}/{subtype}/size")
@@ -95,22 +93,26 @@ class raw_object(object):
         self.h = ship_size[4]
 
     def get_labelline(self):
-        line = f"{self.type} {self.tx} {self.ty} {self.tz} {self.pos_sx} {self.neg_sx} {self.pos_sy} " +\
-               f"{self.neg_sy} {self.h} {self.tyaw} {self.mx} {self.my} {self.mz} {self.mroll} {self.mpitch} " +\
-               f"{self.myaw} {self.tdis}\n"
+        line = f"{self.type} {self.tx} {self.ty} {self.tz} {self.tyaw} {self.tlinearx} {self.tangularz} " +\
+            f"{self.pos_sx} {self.neg_sx} {self.pos_sy} {self.neg_sy} {self.h} {self.timestamp}\n"
         return line
 
 
 def get_model_state(name):
+    # time = rospy.get_time()
     global g_get_state
-    header = Header()
-    header.stamp = rospy.Time.now()
-    header.frame_id = "world"
+
     try:
         model_state = g_get_state(model_name=name)
     except Exception as e:
         rospy.logerr('Error on calling service: %s', str(e))
         return
+    time_now = rospy.get_time()
+    # dur_time = rospy.get_time() - time
+    # print(f"read {name} cost {dur_time}s.")
+    header = Header()
+    header.stamp = time_now
+    header.frame_id = "world"
     latest_state = ShipState()
     latest_state.header = header
     latest_state.modelstate = model_state
@@ -141,11 +143,10 @@ def get_objectstate(my_ship_state):
             if dis > 200:
                 continue
             else:
-                ship_object = raw_object(my_ship_state)
+                ship_object = raw_object()
                 ship_object.read_target_from_state('ship', obj_ship_state)
                 ship_object.read_basicinfos('ships', ship_type)
                 objectsList.append(ship_object)
-                # print(f"{ship_type}, ({obj_x}, {obj_y})\n")
     # objects
     objects_type = rospy.get_param(f"objects/total_objects_type")
     # get all object states
@@ -162,18 +163,15 @@ def get_objectstate(my_ship_state):
             obj_y = global_position[1] + object_position[0] * \
                 np.sin(global_yaw) + object_position[1] * np.cos(global_yaw)
             dis = gp.euclid_distance(my_x, my_y, obj_x, obj_y)
-            if dis > 200:
+            if dis > 180:
                 continue
             else:
-                object = raw_object(my_ship_state)
+                object = raw_object()
                 object.read_target_from_param(
                     object_type, obj_x, obj_y, global_yaw)
                 object.read_basicinfos('objects', object_type)
                 objectsList.append(object)
-                # print(f"{object_type}, ({obj_x}, {obj_y})\n")
     return objectsList
-    # print(f"my state: ({my_x},{my_y})")
-    # print(f"target number: {len(objectsList)}")
 
 
 def linestr(key, value):
@@ -273,14 +271,30 @@ def write_raw_label(index, object_list):
             f.write(object.get_labelline())
 
 
-def write_basic_infos(index, mystate, ros_timestamp, camera_timestamp):
+def write_basic_infos(index, mystate, camera_timestamp, velodyne_timestamp, livox_timestamp):
     info_dir = os.path.join(split_dir, 'info')
     info_filename = os.path.join(info_dir, '%06d.txt' % (index))
+    mtimestamp = mystate.header.stamp
+    mx = mystate.modelstate.pose.position.x
+    my = mystate.modelstate.pose.position.y
+    mz = mystate.modelstate.pose.position.z
+    mqx = mystate.modelstate.pose.orientation.x
+    mqy = mystate.modelstate.pose.orientation.y
+    mqz = mystate.modelstate.pose.orientation.z
+    mqw = mystate.modelstate.pose.orientation.w
+    mrpy = gp.quart_to_rpy(mqx, mqy, mqz, mqw)
+    mroll = mrpy[0]
+    mpitch = mrpy[1]
+    myaw = mrpy[2]
+
+    mlinearx = mystate.modelstate.twist.linear.x
+    mangularz = mystate.modelstate.twist.angular.z
+    motion_label = f"{mx} {my} {mz} {mroll} {mpitch} {myaw} {mlinearx} {mangularz} {mtimestamp}"
     with open(info_filename, 'w') as f:
-        f.write(linestr('x', mystate.modelstate.pose.position.x))
-        f.write(linestr('y', mystate.modelstate.pose.position.y))
-        f.write(linestr('ros_timestamp', ros_timestamp))
         f.write(linestr('camera_timestamp', camera_timestamp))
+        f.write(linestr('velodyne_timestamp', velodyne_timestamp))
+        f.write(linestr('livox_timestamp', livox_timestamp))
+        f.write(linestr('motion', motion_label))
 
 
 rgb_ram = None
@@ -298,7 +312,9 @@ index = 0
 def collect(rgb_msg, camera_info, velo_msg, livox_msg):
     rospy.loginfo("writing===camera time: %s, velo time: %s, livox time: %s.", rgb_msg.header.stamp.to_sec(
     ), velo_msg.header.stamp.to_sec(), livox_msg.header.stamp.to_sec())
-    ros_time = rospy.get_time()
+    camera_timestamp = rgb_msg.header.stamp.to_sec()
+    velodyne_timestamp = velo_msg.header.stamp.to_sec()
+    livox_timestamp = livox_msg.header.stamp.to_sec()
     mystate = get_mystate()
     objectstate_list = get_objectstate(mystate)
     global index
@@ -307,8 +323,8 @@ def collect(rgb_msg, camera_info, velo_msg, livox_msg):
     write_velodyne(index, velo_msg)
     write_livox(index, livox_msg)
     write_raw_label(index, objectstate_list)
-    write_basic_infos(index, mystate, ros_time,
-                      rgb_msg.header.stamp.to_sec())
+    write_basic_infos(index, mystate, camera_timestamp,
+                      velodyne_timestamp, livox_timestamp)
     index += 1
     print("write once!")
 
@@ -337,8 +353,8 @@ def callback(rgb_msg, camera_info):
         rgb_current_time = rgb_msg.header.stamp.to_sec()
         velo_time = velodyne_ram.header.stamp.to_sec()
         livox_time = livox_ram.header.stamp.to_sec()
-        print(
-            f"c_ltime:{rgb_last_time}, c_ctime:{rgb_current_time}, vtime:{velo_time}, ltime:{livox_time}")
+        # print(
+        #     f"c_ltime:{rgb_last_time}, c_ctime:{rgb_current_time}, vtime:{velo_time}, ltime:{livox_time}")
         min_time = min(velo_time, livox_time)
         max_time = max(velo_time, livox_time)
         durrent_time = max_time - min_time
